7bd1b7374c5dd83f2c81933b7bd80d45e51ee714
michealokosun <michealokosun6@gmail.com>
2025-05-07 06:28:20 +0100

    added validations to the forms

---
+++ app/error.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+function errorpage() {
+  return (
+    <>
+      <h1>error page</h1>
+    </>
+  );
+}
+
+export default errorpage;
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,6 +9,7 @@ import {
 } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { Button } from "@/app/ui/button";
+import { Editinvoice } from "@/app/lib/actions";
 
 export default function EditInvoiceForm({
   invoice,
@@ -17,10 +18,10 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
-  console.log(invoice);
+  const updateInvoiceWithId = Editinvoice.bind(null, invoice.id);
 
   return (
-    <form>
+    <form action={updateInvoiceWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
