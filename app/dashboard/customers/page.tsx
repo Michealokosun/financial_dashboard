76247efe6d3369ddc0b769f05be13fb7521840e6
michealokosun <michealokosun6@gmail.com>
2025-05-13 19:23:10 +0100

    finished the project

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,21 @@
-export default function page() {
-  return <p>customes page</p>;
+import { fetchAllCustomers, fetchFilteredCustomers } from "@/app/lib/data";
+import CustomersTable from "@/app/ui/customers/table";
+type SearchParams = Promise<{ [key: string]: string }>;
+
+export default async function page(props: { searchParams: SearchParams }) {
+  const searchParams = await props.searchParams;
+  let customers;
+  const query = searchParams.query || "";
+  console.log(query);
+  if (query == "") {
+    customers = await fetchAllCustomers();
+  } else {
+    customers = await fetchFilteredCustomers(query);
+  }
+
+  return (
+    <div>
+      <CustomersTable customers={customers} />
+    </div>
+  );
 }
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -178,6 +178,32 @@ export async function fetchCustomers() {
   }
 }
 
+export async function fetchAllCustomers() {
+  const data = await sql<CustomersTableType[]>`
+  SELECT
+  customers.id,
+  customers.name,
+  customers.email,
+  customers.image_url,
+  COUNT(invoices.id) AS total_invoices,
+  SUM(CASE WHEN invoices.status = 'pending' THEN invoices.amount ELSE 0 END) AS total_pending,
+  SUM(CASE WHEN invoices.status = 'paid' THEN invoices.amount ELSE 0 END) AS total_paid
+FROM customers
+LEFT JOIN invoices ON customers.id = invoices.customer_id
+GROUP BY customers.id, customers.name, customers.email, customers.image_url
+ORDER BY customers.name ASC;
+
+  `;
+
+  const customers = data.map((customer) => ({
+    ...customer,
+    total_pending: formatCurrency(customer.total_pending),
+    total_paid: formatCurrency(customer.total_paid),
+  }));
+
+  return customers;
+}
+
 export async function fetchFilteredCustomers(query: string) {
   try {
     const data = await sql<CustomersTableType[]>`
